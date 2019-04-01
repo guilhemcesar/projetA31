a6e280e4a849cb032306bafcc818c3cbc3eca75c
Guilhem CESAR <guilhem.cesar2@gmail.com>
2019-04-01 15:48:21 +0200

    exercice 2

--- src/Arbre.java
+++ src/Arbre.java
@@ -1,2 +1,30 @@
-public class Arbre {
+class Arbre {
+
+    int valeur;
+    Arbre gauche;
+    Arbre droite;
+
+    Arbre(int i){
+        valeur  = i;
+    }
+
+    Arbre(int i, Arbre g, Arbre d){
+        valeur = i;
+        gauche = g;
+        droite = d;
+    }
+
+    int getValeur(){
+        return valeur;
+    }
+
+    Arbre getSousArbreDroit(){
+        return droite;
+    }
+
+    Arbre getSousArbreGauche(){
+        return gauche;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
         System.out.println("Hello World!");
 
+        Arbre b = new Arbre(2, new Arbre(1), new Arbre(4, new Arbre(3), new Arbre(5)));
+        Arbre c = new Arbre(10, new Arbre(8), new Arbre(12));
+        Arbre a = new Arbre(6, b, c);
 
     }
 }
