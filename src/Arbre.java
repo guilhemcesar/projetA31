e15f1c8a9efcd85b0d001afb3ee15b91a2b614f3
Guilhem CESAR <guilhem.cesar2@gmail.com>
2019-04-02 14:07:29 +0200

    exercice 4

--- src/Arbre.java
+++ src/Arbre.java
@@ -1,4 +1,4 @@
-class Arbre {
+public class Arbre {
 
     int valeur;
     Arbre gauche;
@@ -26,5 +26,87 @@ class Arbre {
         return gauche;
     }
 
+    void parcourPrefixe(){
 
+        System.out.println(getValeur());
+        if (getSousArbreGauche() != null)
+            getSousArbreGauche().parcourPrefixe();
+        if(getSousArbreDroit() != null)
+            getSousArbreDroit().parcourPrefixe();
+    }
+
+    void parcourInfixe(){
+
+        if (getSousArbreGauche() != null)
+            getSousArbreGauche().parcourInfixe();
+        System.out.println(getValeur());
+        if(getSousArbreDroit() != null)
+            getSousArbreDroit().parcourInfixe();
+    }
+
+    void parcourPostfixe(){
+
+        if (getSousArbreGauche() != null)
+            getSousArbreGauche().parcourPostfixe();
+        if(getSousArbreDroit() != null)
+            getSousArbreDroit().parcourPostfixe();
+        System.out.println(getValeur());
+    }
+
+    boolean arbresEgaux(Arbre a, Arbre b) {
+
+        if ((a == null) && (b == null))
+            return true;
+        if ((a == null) && (b != null))
+            return false;
+        if ((a != null) && (b == null))
+            return false;
+        if (a.getValeur() != b.getValeur())
+            return false;
+        return (arbresEgaux(a.getSousArbreGauche(), b.getSousArbreGauche()) && arbresEgaux(a.getSousArbreDroit(), b.getSousArbreDroit()));
+    }
+
+    int hauteur(Arbre a) {
+        if (a == null)
+            return 0;
+        else
+            return (1 + Math.max(hauteur(a.getSousArbreGauche()), hauteur(a.getSousArbreDroit())));
+    }
+
+    boolean estABR(Arbre a) {
+        if (a == null)
+            return true;
+        if ((a.getSousArbreGauche() != null) && (a.getSousArbreGauche().getValeur() > a.getValeur()))
+            return false;
+        if ((a.getSousArbreDroit() != null) && (a.getValeur() > a.getSousArbreDroit().getValeur()))
+            return false;
+        return (estABR(a.getSousArbreGauche()) && estABR(a.getSousArbreDroit()));
+    }
+
+    void insertion(int value) {
+        if (value == getValeur())
+            return;  // la valeur est deja dans l'arbre
+        if (value < getValeur()) {
+            if (getSousArbreGauche() != null)
+                getSousArbreGauche().insertion(value);
+            else
+                gauche = new Arbre(value);
+        }
+        if (value > getValeur()) {
+            if (getSousArbreDroit() != null)
+                getSousArbreDroit().insertion(value);
+            else
+                droite = new Arbre(value);
+        }
+    }
+
+    boolean recherche(int value) {
+        if (value == getValeur())
+            return true;
+        if ((value < getValeur()) && (getSousArbreGauche() != null))
+            return (getSousArbreGauche().recherche(value));
+        if ((value > getValeur()) && (getSousArbreDroit() != null))
+            return (getSousArbreDroit().recherche(value));
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,26 @@ public class Main {
         Arbre c = new Arbre(10, new Arbre(8), new Arbre(12));
         Arbre a = new Arbre(6, b, c);
 
+        Arbre test = new Arbre(1);
+        System.out.println(test.gauche);
+        System.out.println(test.getSousArbreGauche());
+
+        a.parcourPostfixe();
+        a.parcourInfixe();
+        a.parcourPrefixe();
+
+        Arbre d = new Arbre(10, new Arbre(8), new Arbre(12));
+
+        System.out.println( a.arbresEgaux(a, b));
+        System.out.println( a.arbresEgaux(c, d));
+
+        System.out.println(a.valeur);
+
+        System.out.println(a);
+
+        System.out.println(a.hauteur(a));
+
+
+
     }
 }
